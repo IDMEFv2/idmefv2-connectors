08a05e0ce9cb4f2f5b27845a4edeed1b78a78497
François Déchelle <fdechelle@teclib.com>
2024-11-05 10:38:13 +0100

    Add options parsing

--- idmefv2/suricata/__main__.py
+++ idmefv2/suricata/__main__.py
@@ -1,6 +1,17 @@
 
 import asyncio
+import argparse
 from idmefv2.suricata.server import EVEServer
 
-server = EVEServer('foo', 'http://127.0.0.1:33333')
-asyncio.run(server.start())
+def parse_options():
+    parser = argparse.ArgumentParser(description='Launch the EVE to IDMEFv2 conversion server')
+    parser.add_argument('-s', '--socket', help='give path to Unix socket used by Suricata to output alerts in EVE format', dest='socket_path')
+    parser.add_argument('-u', '--url', help='URL of IDMEFv2 server receiving converted alerts', dest='idmefv2_url')
+    return parser.parse_args()
+
+def main():
+    options = parse_options()
+    server = EVEServer(options.socket_path, options.idmefv2_url)
+    asyncio.run(server.start())
+
+main()
