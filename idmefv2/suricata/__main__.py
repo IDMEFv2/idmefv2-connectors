d0a0bd1a4830774929e3c41eef583d7666fce953
François Déchelle <fdechelle@teclib.com>
2024-11-06 17:16:57 +0100

    Add logging

--- idmefv2/suricata/__main__.py
+++ idmefv2/suricata/__main__.py
@@ -1,17 +1,22 @@
 
 import asyncio
 import argparse
+import logging
 from idmefv2.suricata.server import EVEServer
 
 def parse_options():
     parser = argparse.ArgumentParser(description='Launch the EVE to IDMEFv2 conversion server')
     parser.add_argument('-s', '--socket', help='give path to Unix socket used by Suricata to output alerts in EVE format', dest='socket_path')
     parser.add_argument('-u', '--url', help='URL of IDMEFv2 server receiving converted alerts', dest='idmefv2_url')
+    parser.add_argument('-d', '--debug', help='set log level to DEBUG', dest='log_debug', action='store_true')
     return parser.parse_args()
 
 def main():
     options = parse_options()
+    level = logging.DEBUG if options.log_debug else logging.INFO
+    logging.basicConfig(level=level)
     server = EVEServer(options.socket_path, options.idmefv2_url)
     asyncio.run(server.start())
 
-main()
+if __name__ == '__main__':
+    main()
