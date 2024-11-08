e5179b7dd5fac603e03b164f8e78ed3d0b0e9576
François Déchelle <fdechelle@teclib.com>
2024-11-08 16:23:39 +0100

    Add more fields

--- idmefv2/suricata/idmefv2_converter.py
+++ idmefv2/suricata/idmefv2_converter.py
@@ -9,6 +9,31 @@ class IDMEFv2Converter(Converter):
         'Version': '2.0.3',
         'ID': idmefv2_uuid,
         'CreateTime': '$.timestamp',
+        'Description' : '$.alert.category',
+        "Analyzer": {
+            "IP": "127.0.0.1",
+            "Name": "suricata",
+            "Model": "Suricata NIDS",
+            "Type": "Cyber",
+            "Category": [
+                "NIDS"
+            ],
+            "Data": [
+                "Network"
+            ],
+            "Method": [
+                "Signature"
+            ]
+        },
+        'Source': {
+            'IP': '$.src_ip',
+            'Port': '$.src_port',
+            'Protocol': '$.proto',
+        },
+        'Target': {
+            'IP': '$.dest_ip',
+            'Port': '$.dest_port',
+        },
     }
 
     def __init__(self):
