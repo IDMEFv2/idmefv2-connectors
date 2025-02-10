97d9e9c76066d51c72ab78f2ac5ba19a975f238b
François Déchelle <fdechelle@teclib.com>
2025-02-10 14:40:19 +0100

    Add loading of suricata config file

--- idmefv2/connectors/suricata/__main__.py
+++ idmefv2/connectors/suricata/__main__.py
@@ -3,6 +3,7 @@ import argparse
 import logging
 import sys
 from configparser import ConfigParser
+import yaml
 from idmefv2.connectors.suricata.eveserver import EVESocketServer, EVEFileServer
 
 class Configuration(ConfigParser):
@@ -16,6 +17,17 @@ def parse_options():
     parser.add_argument('-c', '--conf', help='give configuration file', dest='conf_file')
     return parser.parse_args()
 
+
+def find_eve_output(filename):
+    with open(filename) as f:
+        cfg = yaml.safe_load(f)
+        r = [x for x in cfg['outputs'] if 'eve-log' in x]
+        el= r[0]['eve-log']
+        if el['enabled']:
+            return (el['filetype'], el['filename'])
+    logging.error('error loading Suricata config file')
+    sys.exit(1)
+
 def main():
     options = parse_options()
     config = Configuration(options.conf_file)
@@ -25,18 +37,18 @@ def main():
 
     idmefv2_url = config.get('idmefv2', 'url')
 
-    server = None
-    eve_output = config.get('suricata', 'eve')
-    if eve_output == 'unix_stream':
-        socket_path = config.get('suricata', 'unixsocket')
-        server = EVESocketServer(url = idmefv2_url, path = socket_path)
-    elif eve_output == 'regular':
-        filename = config.get('suricata', filename)
-        server = EVEFileServer(url = idmefv2_url, path = filename)
+    suricata_config = config.get('suricata', 'config')
+    filetype, filename = find_eve_output(suricata_config)
 
-    if server is None:
-        print('configuration option suricata.eve must be one of [unix_stream, regular]', file=sys.stderr)
+    if filetype not in ['unix_stream', 'regular']:
+        logging.error("configuration option suricata.eve must be one of ['unix_stream', 'regular']")
         sys.exit(1)
+
+    server = None
+    if filetype == 'unix_stream':
+        server = EVESocketServer(url = idmefv2_url, path = filename)
+    elif filetype == 'regular':
+        server = EVEFileServer(url = idmefv2_url, path = filename)
 
     server.run()
 
