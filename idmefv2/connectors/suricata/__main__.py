588e579cffcef0b73143b7f83f16b1177b713a44
François Déchelle <fdechelle@teclib.com>
2025-04-22 17:46:43 +0200

    Factorize duplicated code

--- idmefv2/connectors/suricata/__main__.py
+++ idmefv2/connectors/suricata/__main__.py
@@ -1,26 +1,12 @@
 '''
 Main for Suricata connector
 '''
-import argparse
 import logging
 import os.path
 import sys
-from configparser import ConfigParser
 import yaml
 from .eveserver import EVESocketServer, EVEFileServer
-from ..idmefv2client import IDMEFv2Client
-
-def parse_options():
-    '''
-    Parse command line options
-
-    Returns:
-        Namespace: parsed command line options
-    '''
-    parser = argparse.ArgumentParser(description='Launch the EVE to IDMEFv2 conversion server')
-    parser.add_argument('-c', '--conf', help='give configuration file', dest='conf_file')
-    return parser.parse_args()
-
+from ..baseconnector import BaseConnector
 
 def find_eve_output(filename: str):
     '''
@@ -58,29 +44,12 @@ def find_eve_output(filename: str):
             filename = os.path.join(default_log_dir, filename)
         return (filetype, filename)
 
-def main():
-    '''
-    Main function:
-        - read configuration file
-        - set logging level
-        - creates the IDMEFv2 HTTP client
-        - parse Suricata configuration file
-        - according to EVE output configuration, launch the EVE server
-    '''
-    options = parse_options()
-    config = ConfigParser()
-    config.read(options.conf_file)
+def _main():
+    suricata_connector = BaseConnector('suricata')
 
-    level = config.get('logging', 'level', fallback='INFO')
-    logging.basicConfig(level=level)
     log = logging.getLogger('suricata-connector')
 
-    idmefv2_url = config.get('idmefv2', 'url')
-    idmefv2_login = config.get('idmefv2', 'login', fallback=None)
-    idmefv2_password = config.get('idmefv2', 'password', fallback=None)
-    idmefv2_client = IDMEFv2Client(url=idmefv2_url, login=idmefv2_login, password=idmefv2_password)
-
-    suricata_config = config.get('suricata', 'config')
+    suricata_config = suricata_connector.config.get('suricata', 'config')
     filetype, filename = find_eve_output(suricata_config)
     accepted_filetypes = ['unix_stream', 'regular']
     if filetype not in accepted_filetypes:
@@ -89,11 +58,11 @@ def main():
 
     server = None
     if filetype == 'unix_stream':
-        server = EVESocketServer(idmefv2_client, filename)
+        server = EVESocketServer(suricata_connector.idmefv2_client, filename)
     elif filetype == 'regular':
-        server = EVEFileServer(idmefv2_client, filename)
+        server = EVEFileServer(suricata_connector.idmefv2_client, filename)
 
     server.run()
 
 if __name__ == '__main__':
-    main()
+    _main()
