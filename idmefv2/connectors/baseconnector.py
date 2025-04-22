57155e4617f65c85ece1b7d5dcb36ca5db51d486
François Déchelle <fdechelle@teclib.com>
2025-04-22 17:46:43 +0200

    Factorize duplicated code

---
+++ idmefv2/connectors/baseconnector.py
@@ -0,0 +1,48 @@
+'''
+Base connector
+'''
+import argparse
+from configparser import ConfigParser
+import logging
+from .idmefv2client import IDMEFv2Client
+
+class BaseConnector:
+    '''
+    Base class for connectors:
+        - parse command line
+        - read configuration file
+        - set logging level
+        - creates the IDMEFv2 HTTP client
+    '''
+
+    def parse_options(self, name: str):
+        '''
+        Parse command line options
+
+        Returns:
+            Namespace: parsed command line options
+        '''
+        description = f"Launch the {name.capitalize()} to IDMEFv2 connector"
+        parser = argparse.ArgumentParser(description=description)
+        parser.add_argument('-c', '--conf', help='give configuration file', dest='conf_file')
+        return parser.parse_args()
+
+    def __init__(self, name: str):
+        '''
+        Main function:
+            - read configuration file
+            - set logging level
+            - creates the IDMEFv2 HTTP client
+            - launch the log file poller
+        '''
+        options = self.parse_options(name)
+        self.config = ConfigParser()
+        self.config.read(options.conf_file)
+
+        level = self.config.get('logging', 'level', fallback='INFO')
+        logging.basicConfig(level=level)
+
+        url = self.config.get('idmefv2', 'url')
+        login = self.config.get('idmefv2', 'login', fallback=None)
+        password = self.config.get('idmefv2', 'password', fallback=None)
+        self.idmefv2_client = IDMEFv2Client(url=url, login=login, password=password)
--- idmefv2/connectors/wazuh/__main__.py
+++ idmefv2/connectors/wazuh/__main__.py
@@ -1,34 +1,26 @@
 '''
 Main for Wazuh connector
 '''
-import argparse
-from configparser import ConfigParser
 import json
 import logging
 import sys
 from .wazuhconverter import WazuhConverter
-from ..idmefv2client import IDMEFv2Client
 from ..filetailer import FileTailer
+from ..baseconnector import BaseConnector
 
-def parse_options():
-    '''
-    Parse command line options
+def _main():
+    wazuh_connector = BaseConnector('wazuh')
 
-    Returns:
-        Namespace: parsed command line options
-    '''
-    parser = argparse.ArgumentParser(description='Launch the Wazuh to IDMEFv2 connector')
-    parser.add_argument('-c', '--conf', help='give configuration file', dest='conf_file')
-    return parser.parse_args()
-
-def _run(wazuh_log_path: str, idmefv2_client: IDMEFv2Client):
     log = logging.getLogger('wazuh-connector')
+
+    wazuh_log_path = wazuh_connector.config.get('wazuh', 'logfile')
+    log.info("Tailing from file %s", wazuh_log_path)
 
     ft = FileTailer(wazuh_log_path)
     try:
         ft.wait_for_file()
     except FileNotFoundError:
-        log.error("cannot read file %s", wazuh_log_path)
+        log.critical("cannot read file %s", wazuh_log_path)
         sys.exit(1)
 
     converter = WazuhConverter()
@@ -37,35 +29,8 @@ def _run(wazuh_log_path: str, idmefv2_client: IDMEFv2Client):
         log.debug("received %s", str(line))
         wazuh_alert = json.loads(line)
         (converted, idmefv2_alert) = converter.convert(wazuh_alert)
-
         if converted:
-            idmefv2_client.post(idmefv2_alert)
-
-def main():
-    '''
-    Main function:
-        - read configuration file
-        - set logging level
-        - creates the IDMEFv2 HTTP client
-        - launch the log file poller
-    '''
-    options = parse_options()
-    config = ConfigParser()
-    config.read(options.conf_file)
-
-    level = config.get('logging', 'level', fallback='INFO')
-    logging.basicConfig(level=level)
-    log = logging.getLogger('wazuh-connector')
-
-    idmefv2_url = config.get('idmefv2', 'url')
-    idmefv2_login = config.get('idmefv2', 'login', fallback=None)
-    idmefv2_password = config.get('idmefv2', 'password', fallback=None)
-    idmefv2_client = IDMEFv2Client(url=idmefv2_url, login=idmefv2_login, password=idmefv2_password)
-
-    wazuh_log_path = config.get('wazuh', 'logfile')
-    log.info("Tailing from file %s", wazuh_log_path)
-
-    _run(wazuh_log_path, idmefv2_client)
+            wazuh_connector.idmefv2_client.post(idmefv2_alert)
 
 if __name__ == '__main__':
-    main()
+    _main()
