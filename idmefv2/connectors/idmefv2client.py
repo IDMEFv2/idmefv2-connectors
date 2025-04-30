1a1d1467bf802110f3fb25fc063f0abc05634dd2
François Déchelle <fdechelle@teclib.com>
2025-04-30 12:03:29 +0200

    Refactored connector code

--- idmefv2/connectors/idmefv2client.py
+++ idmefv2/connectors/idmefv2client.py
@@ -1,10 +1,7 @@
 '''
 A HTTP client POSTing IDMEFv2 messages and logging response
 '''
-import logging
 import requests
-
-log = logging.getLogger('idmefv2-client')
 
 # pylint: disable=too-few-public-methods
 class IDMEFv2Client:
@@ -23,9 +20,8 @@ class IDMEFv2Client:
         Args:
             idmefv2 (dict): the IDMEFv2 message, supposed to be valid
         '''
-        log.info("sending IDMEFv2 alert %s", str(idmefv2))
         kwargs = {'json' : idmefv2, 'timeout' : 1.0}
         if self.login is not None and self.password is not None:
             kwargs['auth'] = (self.login, self.password)
         r = requests.post(self.url, **kwargs)
-        log.debug("got response %s", r)
+        r.raise_for_status()
--- idmefv2/connectors/wazuh/__main__.py
+++ idmefv2/connectors/wazuh/__main__.py
@@ -1,46 +1,11 @@
 '''
 Main for Wazuh connector
 '''
-import json
-import logging
-import sys
 from .wazuhconverter import WazuhConverter
-from ..filetailer import FileTailer
-from ..connector import Connector
-
-class WazuhConnector(Connector):
-    '''
-    Class for Wazuh connector
-    '''
-    def __init__(self):
-        super().__init__('wazuh')
-        self.log_path = self.config.get('wazuh', 'logfile')
-        self.converter = WazuhConverter()
-
-    def run(self):
-        '''
-        Run the connector: loop
-            - receiving JSON alert
-            - converting alert to IDMEFv2
-            - sending converted alert to IDMEFv2 server
-        '''
-        log = logging.getLogger('wazuh-connector')
-
-        log.info("Tailing from file %s", self.log_path)
-
-        ft = FileTailer(self.log_path)
-        try:
-            ft.wait_for_file()
-        except FileNotFoundError:
-            log.critical("cannot read file %s", self.log_path)
-            sys.exit(1)
-
-        for line in ft.tail():
-            log.debug("received %s", line)
-            wazuh_alert = json.loads(line)
-            (converted, idmefv2_alert) = self.converter.convert(wazuh_alert)
-            if converted:
-                self.idmefv2_client.post(idmefv2_alert)
+from ..connector import Configuration, LogFileRunner
 
 if __name__ == '__main__':
-    WazuhConnector().run()
+    cfg = Configuration('wazuh')
+    log_file_path = cfg.get('wazuh', 'logfile')
+    runner = LogFileRunner(cfg, WazuhConverter(), log_file_path)
+    runner.run()
