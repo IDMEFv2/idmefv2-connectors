f069c6f8b8989bff2d23273c69b1697bff55dd37
François Déchelle <fdechelle@teclib.com>
2025-06-12 15:30:52 +0200

    Add exception catching on POST to server. Close #5

--- idmefv2/connectors/idmefv2client.py
+++ idmefv2/connectors/idmefv2client.py
@@ -9,9 +9,10 @@ class IDMEFv2Client:
     Class storing client configuration and sending IDMEFv2 messages
     '''
     def __init__(self, *, url: str, login: str = None, password: str = None):
-        self.url = url
-        self.login = login
-        self.password = password
+        self._url = url
+        self._session = requests.Session()
+        if login is not None and password is not None:
+            self._session.auth = (login, password)
 
     def post(self, idmefv2: dict):
         '''
@@ -21,7 +22,5 @@ class IDMEFv2Client:
             idmefv2 (dict): the IDMEFv2 message, supposed to be valid
         '''
         kwargs = {'json' : idmefv2, 'timeout' : 1.0}
-        if self.login is not None and self.password is not None:
-            kwargs['auth'] = (self.login, self.password)
-        r = requests.post(self.url, **kwargs)
+        r = self._session.post(self._url, **kwargs)
         r.raise_for_status()
