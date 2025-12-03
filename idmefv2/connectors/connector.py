ddc67565b176d31150988cdccaa9f9eb8214815f
François Déchelle <fdechelle@teclib.com>
2025-12-03 15:25:08 +0100

    Separate command line argument parsing and configuration in 2 classes

--- idmefv2/connectors/connector.py
+++ idmefv2/connectors/connector.py
@@ -2,7 +2,7 @@
 Base connector
 '''
 import abc
-import argparse
+from argparse import ArgumentParser, Namespace
 from configparser import ConfigParser
 import json
 import logging
@@ -13,51 +13,33 @@ from .idmefv2client import IDMEFv2Client
 from .jsonconverter import JSONConverter
 from .filetailer import FileTailer
 
-class Configuration:
+class ConnectorArgumentParser(ArgumentParser):
+    '''
+    Base class for connector command line argument parsing:
+        - add -c/--conf option to give configuration file
+    '''
+    def __init__(self, name: str):
+        description = f"Launch the {name.capitalize()} to IDMEFv2 connector"
+        super().__init__(description=description)
+        self.add_argument('-c', '--conf', help='give configuration file', dest='conf_file')
+
+class Configuration(ConfigParser):
     '''
     Base class for connectors configuration:
-        - parse command line
         - read configuration file
     '''
 
-    def parse_options(self, name: str):
+    def __init__(self, opts: Namespace):
         '''
-        Parse command line options
-
-        Returns:
-            Namespace: parsed command line options
-        '''
-        description = f"Launch the {name.capitalize()} to IDMEFv2 connector"
-        parser = argparse.ArgumentParser(description=description)
-        parser.add_argument('-c', '--conf', help='give configuration file', dest='conf_file')
-        return parser.parse_args()
-
-    def __init__(self, name: str):
-        '''
-        Main function:
+        Constructor:
             - read configuration file
         '''
-        self.name = name
-        options = self.parse_options(name)
-        self._config = ConfigParser()
-        self._config.read(options.conf_file)
+        super().__init__()
+        self.read(opts.conf_file)
 
-    def get(self, section: str, option: str, **kwargs):
-        '''
-        Returns configuration element
-
-        Args:
-            section (str): the configuration section
-            option (str): the configuration option
-
-        Returns:
-            the corresponding value if exists, fallback if not
-        '''
-        return self._config.get(section, option, **kwargs)
-
-class Runner(abc.ABC):
+class Connector(abc.ABC):
     '''
-    Base class for running connectors
+    Base class for connectors
     '''
 
     def __init__(self, cfg: Configuration, converter: JSONConverter):
@@ -107,7 +89,7 @@ class Runner(abc.ABC):
         '''
         raise NotImplementedError
 
-class LogFileRunner(Runner):
+class LogFileConnector(Connector):
     '''
     Runner for log file
     '''
--- idmefv2/connectors/suricata/__main__.py
+++ idmefv2/connectors/suricata/__main__.py
@@ -5,53 +5,45 @@ import os.path
 import socketserver
 import yaml
 from .suricataconverter import SuricataConverter
-from ..connector import Configuration, Runner, LogFileRunner
+from ..connector import ConnectorArgumentParser, Configuration, Connector, LogFileConnector
 from ..jsonconverter import JSONConverter
 
-class SuricataConfiguration(Configuration):
+def find_eve_output(suricata_config_file: str):
     '''
-    Class for Suricata connector configuration
+    Reads Suricata YAML configuration file in order to find EVE output configuration fields
+
+    Raises:
+        KeyError: if needed keys not found in configuration fields
+        ValueError: if EVE output disabled
+
+    Returns:
+        (filetype, filename) tuple if found and enabled
     '''
-    def __init__(self):
-        super().__init__('suricata')
-        (self.filetype, self.filename) = self._find_eve_output()
+    with open(suricata_config_file, 'rb') as f:
+        suricata_config = yaml.safe_load(f)
+        # suricata_config['outputs'] is a list of dict
+        outputs = (output for output in suricata_config['outputs'] if 'eve-log' in output)
+        eve_log_output = next(outputs, None)
+        if eve_log_output is None:
+            raise KeyError('eve-log not found in outputs')
+        eve_log = eve_log_output['eve-log']
+        enabled = eve_log.get('enabled', False)
+        filetype = eve_log.get('filetype')
+        filename = eve_log.get('filename')
+        if not enabled:
+            raise ValueError('EVE output is disabled')
+        if filetype is None or filename is None:
+            raise KeyError('filetype or filename not found')
+        accepted_filetypes = ['unix_stream', 'regular']
+        if filetype not in accepted_filetypes:
+            raise ValueError(f"option suricata.eve must be one of {accepted_filetypes}")
+        if not os.path.isabs(filename):
+            default_log_dir = suricata_config.get('default-log-dir')
+            if default_log_dir is None:
+                raise KeyError('filename is relative and no default-log-dir defined')
+            filename = os.path.join(default_log_dir, filename)
 
-    def _find_eve_output(self):
-        '''
-        Reads Suricata YAML configuration file in order to find EVE output configuration fields
-
-        Raises:
-            KeyError: if needed keys not found in configuration fields
-            ValueError: if EVE output disabled
-
-        Returns:
-            (filetype, filename) tuple if found and enabled
-        '''
-        with open(self.get('suricata', 'config'), 'rb') as f:
-            suricata_config = yaml.safe_load(f)
-            # suricata_config['outputs'] is a list of dict
-            outputs = (output for output in suricata_config['outputs'] if 'eve-log' in output)
-            eve_log_output = next(outputs, None)
-            if eve_log_output is None:
-                raise KeyError('eve-log not found in outputs')
-            eve_log = eve_log_output['eve-log']
-            enabled = eve_log.get('enabled', False)
-            filetype = eve_log.get('filetype')
-            filename = eve_log.get('filename')
-            if not enabled:
-                raise ValueError('EVE output is disabled')
-            if filetype is None or filename is None:
-                raise KeyError('filetype or filename not found')
-            accepted_filetypes = ['unix_stream', 'regular']
-            if filetype not in accepted_filetypes:
-                raise ValueError(f"option suricata.eve must be one of {accepted_filetypes}")
-            if not os.path.isabs(filename):
-                default_log_dir = suricata_config.get('default-log-dir')
-                if default_log_dir is None:
-                    raise KeyError('filename is relative and no default-log-dir defined')
-                filename = os.path.join(default_log_dir, filename)
-
-            return (filetype, filename)
+        return (filetype, filename)
 
 class EVEStreamRequestHandler(socketserver.StreamRequestHandler):
     '''
@@ -61,12 +53,12 @@ class EVEStreamRequestHandler(socketserver.StreamRequestHandler):
         data = self.rfile.readline().strip()
         self.server.alert(data)
 
-class SuricataUnixSocketRunner(Runner, socketserver.UnixStreamServer):
+class SuricataUnixSocketConnector(Connector, socketserver.UnixStreamServer):
     '''
     Connector runner for Unix socket
     '''
     def __init__(self, cfg: Configuration, converter: JSONConverter, socket_path: str):
-        super(Runner).__init__(cfg, converter)
+        super(Connector).__init__(cfg, converter)
         super(socketserver.UnixStreamServer).__init__(socket_path, EVEStreamRequestHandler)
         self._socket_path = socket_path
 
@@ -75,11 +67,13 @@ class SuricataUnixSocketRunner(Runner, socketserver.UnixStreamServer):
         self.serve_forever()
 
 if __name__ == '__main__':
-    suricata_cfg = SuricataConfiguration()
+    opts = ConnectorArgumentParser('suricata').parse_args()
+    suricata_cfg = Configuration(opts)
+    (suricata_filetype, suricata_filename) = find_eve_output(suricata_cfg.get('suricata', 'config'))
     suricata_converter = SuricataConverter()
-    if suricata_cfg.filetype == 'unix_stream':
-        runner = SuricataUnixSocketRunner(suricata_cfg, suricata_converter, suricata_cfg.filename)
-        runner.run()
-    elif suricata_cfg.filetype == 'regular':
-        runner = LogFileRunner(suricata_cfg, suricata_converter, suricata_cfg.filename)
-        runner.run()
+    if suricata_filetype == 'unix_stream':
+        connector = SuricataUnixSocketConnector(suricata_cfg, suricata_converter, suricata_filename)
+        connector.run()
+    elif suricata_filetype == 'regular':
+        connector = LogFileConnector(suricata_cfg, suricata_converter, suricata_filename)
+        connector.run()
