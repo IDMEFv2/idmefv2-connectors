01c96e73028059590a8e4636ab1844f2dee1229d
François Déchelle <fdechelle@teclib.com>
2025-12-04 10:43:16 +0100

    Remove reading the suricata yaml config file and move corresponding configuration to connector .conf file

--- idmefv2/connectors/suricata/__main__.py
+++ idmefv2/connectors/suricata/__main__.py
@@ -1,49 +1,10 @@
 '''
 Main for Suricata connector
 '''
-import os.path
 import socketserver
-import yaml
 from .suricataconverter import SuricataConverter
 from ..connector import ConnectorArgumentParser, Configuration, Connector, LogFileConnector
 from ..jsonconverter import JSONConverter
-
-def find_eve_output(suricata_config_file: str):
-    '''
-    Reads Suricata YAML configuration file in order to find EVE output configuration fields
-
-    Raises:
-        KeyError: if needed keys not found in configuration fields
-        ValueError: if EVE output disabled
-
-    Returns:
-        (filetype, filename) tuple if found and enabled
-    '''
-    with open(suricata_config_file, 'rb') as f:
-        suricata_config = yaml.safe_load(f)
-        # suricata_config['outputs'] is a list of dict
-        outputs = (output for output in suricata_config['outputs'] if 'eve-log' in output)
-        eve_log_output = next(outputs, None)
-        if eve_log_output is None:
-            raise KeyError('eve-log not found in outputs')
-        eve_log = eve_log_output['eve-log']
-        enabled = eve_log.get('enabled', False)
-        filetype = eve_log.get('filetype')
-        filename = eve_log.get('filename')
-        if not enabled:
-            raise ValueError('EVE output is disabled')
-        if filetype is None or filename is None:
-            raise KeyError('filetype or filename not found')
-        accepted_filetypes = ['unix_stream', 'regular']
-        if filetype not in accepted_filetypes:
-            raise ValueError(f"option suricata.eve must be one of {accepted_filetypes}")
-        if not os.path.isabs(filename):
-            default_log_dir = suricata_config.get('default-log-dir')
-            if default_log_dir is None:
-                raise KeyError('filename is relative and no default-log-dir defined')
-            filename = os.path.join(default_log_dir, filename)
-
-        return (filetype, filename)
 
 class EVEStreamRequestHandler(socketserver.StreamRequestHandler):
     '''
@@ -67,9 +28,14 @@ class SuricataUnixSocketConnector(Connector, socketserver.UnixStreamServer):
         self.serve_forever()
 
 if __name__ == '__main__':
+    # pylint: disable=line-too-long
     opts = ConnectorArgumentParser('suricata').parse_args()
     suricata_cfg = Configuration(opts)
-    (suricata_filetype, suricata_filename) = find_eve_output(suricata_cfg.get('suricata', 'config'))
+    suricata_filetype = suricata_cfg.get('suricata', 'filetype')
+    accepted_filetypes = ['unix_stream', 'regular']
+    if suricata_filetype not in accepted_filetypes:
+        raise ValueError(f"option suricata.filetype be one of {accepted_filetypes}")
+    suricata_filename = suricata_cfg.get('suricata', 'filename')
     suricata_converter = SuricataConverter()
     if suricata_filetype == 'unix_stream':
         connector = SuricataUnixSocketConnector(suricata_cfg, suricata_converter, suricata_filename)
