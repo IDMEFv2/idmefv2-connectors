235ce54b8ba4571289f4096d6b9668c17a051480
François Déchelle <fdechelle@teclib.com>
2026-01-13 14:57:14 +0100

    Move configuration to arguments, add verify variable

--- idmefv2/connectors/idmefv2client.py
+++ idmefv2/connectors/idmefv2client.py
@@ -2,17 +2,21 @@
 A HTTP client POSTing IDMEFv2 messages and logging response
 '''
 import requests
+from .connector import Configuration
 
 # pylint: disable=too-few-public-methods
 class IDMEFv2Client:
     '''
     Class storing client configuration and sending IDMEFv2 messages
     '''
-    def __init__(self, *, url: str, login: str = None, password: str = None):
-        self._url = url
+    def __init__(self, cfg: Configuration):
+        self._url = cfg.get('idmefv2', 'url')
         self._session = requests.Session()
+        login = cfg.get('idmefv2', 'login', fallback=None)
+        password = cfg.get('idmefv2', 'password', fallback=None)
         if login is not None and password is not None:
             self._session.auth = (login, password)
+        self._session.verify = cfg.getboolean('idmefv2', 'verify', fallback=True)
 
     def post(self, idmefv2: dict):
         '''
