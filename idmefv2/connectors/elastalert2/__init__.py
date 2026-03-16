0f38f792e71b7c3879a8dd042beff19f7ed74daa
Thierry Bugier <tbugier@teclib.com>
2026-03-16 18:22:46 +0100

    Add elastalert2 idmefv2 alerter; WIP

--- idmefv2/connectors/elastalert2/__init__.py
+++ idmefv2/connectors/elastalert2/__init__.py
@@ -2,3 +2,5 @@
 An elastalert2 alerter, translating match to IDMEFv2 format
 and forwarding translated alerts on HTTP/HTTPS
 '''
+
+from .idmefv2alerter import IDMEFv2Alerter
--- idmefv2/connectors/elastalert2/idmefv2alerter.py
+++ idmefv2/connectors/elastalert2/idmefv2alerter.py
@@ -2,10 +2,12 @@
 Module containing IDMEFv2 alerter
 '''
 from elastalert.alerts import Alerter
-from ..idmefv2client2 import IDMEFv2Client2
+from ..idmefv2client import IDMEFv2Client
 
 class IDMEFv2Alerter(Alerter):
-
+    '''
+    Class for generating IDMEFv2 alerts from elastalert2
+    '''
     required_options = set(['idmefv2_url'])
 
     def __init__(self, rule):
@@ -14,11 +16,10 @@ class IDMEFv2Alerter(Alerter):
         login = self.rule.get('idmefv2_login')
         password = self.rule.get('idmefv2_password')
         verify = self.rule.get('idmefv2_verify', True)
-        self._idmefv2_client = IDMEFv2Client2(url, login=login, password=password, verify=verify)
+        self._idmefv2_client = IDMEFv2Client(url, login=login, password=password, verify=verify)
 
-    def alert(self, matches):
-        for match in matches:
-            self._idmefv2_client.post(match)
+    def alert(self, match):
+        self._idmefv2_client.post(match)
 
     def get_info(self):
         return {'type': 'IDMEFv2 Alerter',
